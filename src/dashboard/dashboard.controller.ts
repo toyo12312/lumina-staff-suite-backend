6e8355daaf005aef4cc0195019306c257ec9a43c
Borjanov Dmitry <33694303+toyo12312@users.noreply.github.com>
2025-09-27 14:33:13 +0300

    Update dashboard.controller.ts

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 
-@Controller('dashboard') // Всі маршрути будуть починатися з /dashboard
+@Controller('dashboard') 
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
-  @Get('summary') // Створюємо новий маршрут /dashboard/summary
+  @Get('summary') /dashboard/summary
   getSummary() {
-    // Викликаємо метод з сервісу, який ми створили раніше
     return this.dashboardService.getSummary();
   }
 }
