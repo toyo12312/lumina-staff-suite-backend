dcf69fea1f8d4aae759560cfc2c66850d7d916c5
Borjanov Dmitry <33694303+toyo12312@users.noreply.github.com>
2026-06-09 02:11:24 +0300

    Update employers.controller.ts

--- src/employers/employers.controller.ts
+++ src/employers/employers.controller.ts
@@ -1,27 +1,57 @@
-Import { Throttle } from '@nestjs/throttler';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { EmployeesService } from './employers.service';
+import { CreateEmployeeDto } from './dto/create-employer.dto';
+import { UpdateEmployeeDto } from './dto/update-employer.dto';
 
-@Throttle({ default: { limit: 2, ttl: 60000 } })
-@Post()
-create(@Body() createEmployeeDto: CreateEmployeeDto) {
-  return this.employeesService.create(createEmployeeDto);
-}
+@Controller('employees') // Всі маршрути будуть починатися з /employees
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) {}
 
-@Get()
-findAll(
-  @Query('search') search?: string,
-  @Query('page') page?: string,
-  @Query('limit') limit?: string,
-) {
-  const pageNum = page ? parseInt(page, 10) : 1;
-  const limitNum = limit ? parseInt(limit, 10) : 10;
-  return this.employeesService.findAll({
-    search,
-    page: pageNum,
-    limit: limitNum,
-  });
-}
+  @Post()
+  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+    return this.employeesService.create(createEmployeeDto);
+  }
 
-@Get(':id')
-findOne(@Param('id', ParseIntPipe) id: number) {
-  return this.employeesService.findOne(id);
+  @Get()
+  findAll(
+    @Query('search') search?: string,
+    @Query('page') page?: string,
+    @Query('limit') limit?: string,
+  ) {
+    const pageNum = page ? parseInt(page, 10) : 1;
+    const limitNum = limit ? parseInt(limit, 10) : 10;
+    return this.employeesService.findAll({
+      search,
+      page: pageNum,
+      limit: limitNum,
+    });
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.employeesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEmployeeDto: UpdateEmployeeDto,
+  ) {
+    return this.employeesService.update(id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.employeesService.remove(id);
+  }
 }
